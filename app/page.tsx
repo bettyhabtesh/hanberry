07aacddf8c3be30df42ba3fd2383b539b44fd887
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2025-12-12 08:17:48 -0600

    feat/ remove components to resolve conflict in apps page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Navbar from '@/components/navbar';
 import HeroSection from '@/components/hero';
 import MakeupIntro from '@/components/welcome';
 import Features from '@/components/features';
-import Book from '@/components/Book';
-import Footer from '@/components/Footer';
-import Contact from '@/components/Contact';
+// import Book from '@/components/Book';
+// import Footer from '@/components/Footer';
+// import Contact from '@/components/Contact';
 
 export default function Home() {
   return (
@@ -12,10 +12,7 @@ export default function Home() {
       <Navbar />
       <HeroSection/>
       <MakeupIntro/>
-      <Features/>   
-      <Book/>
-      <Contact/>
-      <Footer/>
+      <Features/> 
     </div>
   );
 }
